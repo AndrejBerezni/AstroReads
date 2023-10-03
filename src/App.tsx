55159cb45c71e1320c4f8017a21ffb5e3fbf2f0a
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-10-03 15:25:07 +0100

    define page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import Home from "./pages/Home";
+import Trending from "./pages/Trending";
+import About from "./pages/About/About";
+import UserPage from "./pages/UserPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <h1>AstroReads</h1>
+      <Navbar />
+      <Home />
+      <Trending />
+      <About />
+      <UserPage />
     </>
   );
 }
