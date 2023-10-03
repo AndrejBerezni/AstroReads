81f91d531135789ec0b3df8213bd3d3b763d4e0e
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-10-03 16:30:45 +0100

    create and style Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,27 @@ import Trending from "./pages/Trending";
 import About from "./pages/About/About";
 import UserPage from "./pages/UserPage";
 import Navbar from "./components/Navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#29def0",
+      contrastText: "#242424",
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <h1>AstroReads</h1>
       <Navbar />
       <Home />
       <Trending />
       <About />
       <UserPage />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,52 @@
+import * as React from "react";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import HomeIcon from "@mui/icons-material/Home";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import InfoIcon from "@mui/icons-material/Info";
+import "./Navbar.css";
+
+const actions = [
+  { icon: <HomeIcon />, name: "Home" },
+  { icon: <PermIdentityIcon />, name: "User Page" },
+  { icon: <WhatshotIcon />, name: "Trending" },
+  { icon: <InfoIcon />, name: "About" },
+];
+
 function Navbar() {
-  return <h1>Navbar</h1>;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <SpeedDial
+      ariaLabel="SpeedDial controlled open example"
+      sx={{ position: "fixed", top: "32px", left: "32px" }}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+      direction="right"
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={handleClose}
+          sx={{
+            backgroundColor: "#29def0",
+            "&:hover": {
+              backgroundColor: "#f00a60",
+              color: "#29def0",
+            },
+          }}
+        />
+      ))}
+    </SpeedDial>
+  );
 }
 
 export default Navbar;
